8830351dc9ecb9e3096bfe1c83cee445d1dacbb4
iqramqra <masrooriqra@gmail.com>
2020-04-30 16:33:20 -0400

    added styling to the cards

--- frontend/src/components/Drink.jsx
+++ frontend/src/components/Drink.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Card, Icon, Image, Button } from 'semantic-ui-react'
 
 class Drink extends React.Component {
 
@@ -7,12 +8,33 @@ class Drink extends React.Component {
         let {name, image, price} = this.props.drink
 
         return (
-           <div className="drink-card">
-               <h3>{name}</h3>
-               <img src={image} alt={name} />
-               <p>${price}</p>
-               <button>Add to Order</button>
-           </div>
+
+            <Card>
+                <Image src={image} wrapped ui={true} />
+                <Card.Content>
+                <Card.Header>{name} | ${price}</Card.Header>
+                </Card.Content>
+                <Card.Content extra>
+                <a>
+                    {/* <Icon name='user' /> */}
+                    {/* <button>Add to Order</button> */}
+
+                    <Button animated='vertical'>
+                        <Button.Content hidden>Add</Button.Content>
+                        <Button.Content visible>
+                            <Icon name='shop' />
+                        </Button.Content>
+                    </Button>
+                </a>
+                </Card.Content>
+            </Card>
+         
+        //    <div className="drink-card">
+        //        <h3>{name}</h3>
+        //        <img src={image} alt={name} />
+        //        <p>${price}</p>
+        //        <button>Add to Order</button>
+        //    </div>
         )
 
     }
